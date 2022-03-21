d89c3613265b0d7a38f84fd860109b0b47d7f36a
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-21 16:19:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+pip install streamlit
 
+import streamlit as st
+
+def file_selector(self):
+   file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+   if file is not None:
+      data = pd.read_csv(file)
+      return data
+   else:
+      st.text("Please upload a csv file")
