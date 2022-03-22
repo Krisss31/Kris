88433033b310c077e91fcaf36f3bc1ee869d1e2b
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-22 20:58:47 +0100

    Update pompier.py

--- pompier.py
+++ pompier.py
@@ -1,10 +1,24 @@
+import pandas as pd
+import streamlit as st 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import accuracy_score
 
 
 
 def file_selector(self):
-   file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
+   file = st.sidebar.file_uploader("Choose a pikle file", type="csv")
    if file is not None:
-      data = pd.read_csv(file)
-      return data
+      df=pd.read_pickle(file)
+      return df
    else:
-      st.text("Please upload a csv file")
+      st.text("Please upload a pikle file")
