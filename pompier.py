1b78b3d815172774dd155e31b08265370320af72
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-23 21:06:05 +0100

    Update pompier.py

--- pompier.py
+++ pompier.py
@@ -25,7 +25,7 @@ def file_selector():
 
 df = file_selector()
 
-st.st.write(df.head())
+st.write(df.head())
 
 def set_features(df):
    df.features = st.multiselect("Please choose the features including target variable that go into the model", [df.columns] )
