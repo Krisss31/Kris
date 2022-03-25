12b9b36d6d24598878359236a861ea5d2fb6f5e4
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-25 16:58:08 +0100

    Update pompier.py

--- pompier.py
+++ pompier.py
@@ -33,10 +33,17 @@ if st.sidebar.checkbox("Display data", False):
     st.write(df)
 
 # Set features
-if df is not None:
-   features_options = df.columns
-   features = st.multiselect("Please choose the features including target variable that go into the model", features_options)
-   df = df[features]
+#if df is not None:
+   #features_options = df.columns
+   #features = st.multiselect("Please choose the features including target variable that go into the model", features_options)
+   "df = df[features]
+   
+choice_features = st.sidebar.multiselect("Do you want to choice the feature", ("yes", "no"))
+if choice_features = "yes":
+   if df is not None:
+   #features_options = df.columns
+   #features = st.multiselect("Please choose the features including target variable that go into the model", features_options)
+   "df = df[features]
 
 st.write(df)
 
