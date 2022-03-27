2aefc75f5206a0cdab4f37a0903f0f1cc63292fa
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-27 19:00:47 +0200

    Update pompier.py

--- pompier.py
+++ pompier.py
@@ -57,7 +57,7 @@ if df is not None:
 def split(df):
       y = df[target]
       x = df.drop(columns=[target])
-      x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, random_state=0)
+      x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
       return x_train, x_test, y_train, y_test
 
 if df is not None:
