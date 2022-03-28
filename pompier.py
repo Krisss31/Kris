87939bfc31065c037754a30f564e532cc71c4891
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-03-28 16:49:51 +0200

    Update pompier.py

--- pompier.py
+++ pompier.py
@@ -155,8 +155,18 @@ if classifier == "Random Forest":
     #st.write("Precision: ", precision_score(y_test, y_pred))
     #st.write("Recall: ", recall_score(y_test, y_pred))
     metrics(metric)
-
     
+   def Num_pumps(model, property_type, Adress_qualifier, Incident):
+      property_type = st.sidebar.multiselect("Property Type:",df["PropertyType"].unique())
+      Adress_qualifier = st.sidebar.multiselect(Adress Qualifier:",df["AdressQualifier"].unique())
+      Incident = st.sidebar.multiselect("Incident:",df["IncidentType"].unique)
+      x = np.array([property_type,Adress_qualifier,Incident]).reshape(1,3)
+      x = pd.get_dummies(x)
+      return model.predict(x))
+   
+   result = Num_Pumps()
+   st.write(result)
+   
 if classifier == "DecisionTreeClassifier":
     st.sidebar.subheader("Hyperparameters")
     criterion= st.sidebar.multiselect("Criterion",("entropy", "giny"))
@@ -173,3 +183,13 @@ if classifier == "DecisionTreeClassifier":
     #st.write("Precision: ", precision_score(y_test, y_pred))
     #st.write("Recall: ", recall_score(y_test, y_pred))
     metrics(metric)
+
+      
+ def Num_pumps(model, (df.columns)):
+   property_type = st.sidebar.multiselect("Property Type:",df["PropertyType"].unique())
+   Adress_qualifier = st.sidebar.multiselect(Adress Qualifier:",df["AdressQualifier"].unique())
+   Incident = st.sidebar.multiselect("Incident:",df["IncidentType"].unique)
+   x = np.array([property_type,Adress_qualifier,Incident]).reshape(1,3)
+   x = pd.get_dummies(x)
+   return model.predict(x))
+   
