c22794c72f69d423d31f4161ddef4bcd92768d46
Krisss31 <97185932+Krisss31@users.noreply.github.com>
2022-06-06 19:17:59 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,18 +1,7 @@
 import streamlit as st 
 import numpy as np 
-import matplotlib.pyplot as plt
-
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-
-from sklearn.metrics import accuracy_score
-
-from joblib import dump, load
+import pandas as pd
+from joblib import load
 
 
 model = load('Number_of_Pumps_XGB.joblib')
